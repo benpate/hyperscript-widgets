b1b97d5ca8035bacc5f2c196a085492676c0f2b5
Ben Pate <ben@pate.org>
2022-01-18 09:59:34 -0700

    Refactor uploader widget

---
+++ uploader/uploader.hs
@@ -0,0 +1,41 @@
+behavior Uploader(url, accept)
+
+init
+	add .uploader to me
+
+	put 
+	`<form hx-post="${url}" 
+		hx-trigger="change" 
+		hx-swap="none" 
+		hx-encoding="multipart/form-data" 
+		script="on htmx:afterRequest trigger upload:success"
+		hidden="true">
+		<input type="file" id="manual-upload" name="file" accept="${accept}">
+	</form>` 
+	at the start of me
+
+	set element form to the first <form/> in me
+	set element input to the first <input[type="file"]/> in me
+
+	call htmx.process(element form)
+	
+on dragenter
+	halt the event
+	add .highlight
+
+on dragover
+	halt the event
+	add .highlight
+
+on dragleave
+	halt the event
+	remove .highlight
+
+on drop(dataTransfer)
+	halt the event
+	remove .highlight
+	set element input.files to dataTransfer.files
+
+on htmx:xhr:progress
+	log event
+end
